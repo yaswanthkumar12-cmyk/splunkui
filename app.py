5b9b90d11543dca6cb9198e8ec20e6789bf5c626
Yaswanth Kumar <MTL1013@middleware.in>
2025-06-09 15:42:14 +0530

    first commit

--- app.py
+++ app.py
@@ -391,6 +391,10 @@ def download_app(app_id):
 def home():
     return render_template('splunk_index.html')
  
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT in env
+    app.run(host='0.0.0.0', port=port, debug=True)
+
  
